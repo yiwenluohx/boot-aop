276da438b0fffd09cb89529a4948ef5fc71701be
luohx <Lhongxiao@lcchain.com>
2021-11-27 00:51:35 +0800

    新增springboot中aop切面

---
+++ src/main/java/com/study/bootaop/BootAopApplication.java
@@ -0,0 +1,16 @@
+package com.study.bootaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class BootAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/bootaop/BootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.bootaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
